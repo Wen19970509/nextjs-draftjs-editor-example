30ae1feacd0f67bdfae284f280824536972324ad
Wen Chen <peter50710t@gmail.com>
2021-12-19 01:09:08 +0800

    edit eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,4 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    extends: ['next', 'prettier', 'plugin:@typescript-eslint/recommended'],
-    rules: { '@typescript-eslint/no-explicit-any': 'off', 'prefer-const': 'off', 'react/no-unescaped-entities': 'off', 'react-hooks/rules-of-hooks': 'off' },
+    extends: ['next'],
+    rules: { 'react/jsx-no-target-blank': 'off', 'prefer-const': 'off', 'react/no-unescaped-entities': 'off', 'react-hooks/rules-of-hooks': 'off' },
 };
