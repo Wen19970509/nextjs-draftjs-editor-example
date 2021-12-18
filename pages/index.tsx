57c00a26947a05f797434bd2206023151fcd545e
Wen Chen <peter50710t@gmail.com>
2021-12-19 00:59:11 +0800

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,9 @@ const Home = () => {
         <div className='min-h-screen flex flex-col'>
             <h1 className='text-center mt-7 text-indigo-500'>DraftJS Editor Example By WenChen</h1>
             <p className='text-center text-blue-700 my-4 underline decoration-1'>
-                <a href='https://github.com/Wen19970509/nextjs-draftjs-editor-example'>GitHub</a>
+                <a href='https://github.com/Wen19970509/nextjs-draftjs-editor-example' target='_blank'>
+                    GitHub
+                </a>
             </p>
             <div className='px-56 flex-grow flex'>
                 <DraftEditor />
